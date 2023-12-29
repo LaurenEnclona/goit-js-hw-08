b845b07fcda79201ad9f37d188b931c06450c0f1
LaurenEnclona <137889742+LaurenEnclona@users.noreply.github.com>
2023-12-30 02:08:16 +0800

    1st submission

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,31 @@
 import _ from 'lodash';
 
 const form = document.querySelector('.feedback-form');
-const storageKey = 'feedback-form-state';
-
-const throttledSaveToStorage = _.throttle(formData => {
-  localStorage.setItem(storageKey, JSON.stringify(formData));
+const throttledSave = _.throttle(() => {
+  const formData = {
+    email: form.querySelector('input[name="email"]').value,
+    message: form.querySelector('textarea[name="message"]').value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }, 500);
 
-form.addEventListener('input', event => {
-  const email = form.querySelector('input[name="email"]').value;
-  const message = form.querySelector('textarea[name="message"]').value;
-  const formData = { email, message };
+form.addEventListener('input', throttledSave);
 
-  throttledSaveToStorage(formData);
+// Add this code within the script
+const storedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (storedFormData) {
+  form.querySelector('input[name="email"]').value = storedFormData.email;
+  form.querySelector('textarea[name="message"]').value = storedFormData.message;
+}
+
+// Add this code within the script
+form.addEventListener('submit', event => {
+  event.preventDefault(); // Prevent default form submission
+  const formData = {
+    email: form.querySelector('input[name="email"]').value,
+    message: form.querySelector('textarea[name="message"]').value,
+  };
+  console.log(formData); // Display data in console
+  localStorage.removeItem('feedback-form-state');
+  form.reset(); // Clear form fields
 });
